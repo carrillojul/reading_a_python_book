2eb3540e21d696d5e22607b08246df8ac2635bd6
Julio carrillo <carrillojulio470@gmail.com>
2026-06-25 23:37:15 -0500

    mi aporte de hoy

---
+++ input_and_while_loop/greeter.py
@@ -0,0 +1,8 @@
+name = input("please enter your name: ")
+print('hello, ' + name + "!" )
+
+prompt = "if you tell us who you are, we can personalize the message you see"
+prompt += "\nWhat is your first name? "
+
+name_1 = input(prompt)
+print('hello, ' + name + "!" )
---
+++ input_and_while_loop/multiple_of_ten.py
@@ -0,0 +1,6 @@
+number = input("Write a number: ")
+
+if int(number) % 10 == 0:
+    print('it is multiple of ten')
+else:
+    print('it is not multiple')
